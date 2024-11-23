b1b0d8aa13ae2793d422289c7ee70184f71a6b4d
Ratt27 <150338584+Ratt27@users.noreply.github.com>
2024-11-23 06:50:58 +0530

    Add files via upload

---
+++ JSDAY1/class1.js
@@ -0,0 +1,12 @@
+console.log("Hello coder army");
+//let const var
+let num=10;
+console.log(num);
+let ar="Rattriz"
+console.log(ar);
+const ids=20;
+console.log(ids);
+//const=constant(the value will not change again)
+var s=10;
+console.log(s);
+//var= old method and let is new method otherwise similar but it has many disadvantages
